55891c143851959e43d6786bed6a6cea56f55cea
Jonathan Elst <jonathan.elst@outlook.com>
2024-03-05 15:34:41 +0100

    Completed routes for reservations, added :new to routes.rb for nested reservations. Put all the other reservation methods nested inside garages.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,20 +1,22 @@
 class ReservationsController < ApplicationController
   before_action :set_reservation, only: [:show, :destroy]
+  before_action :set_garage, only: [:index, :new, :create]
 
   def show
   end
 
   def index
-    @reservations = Reservation.all
+    @garage = Garage.find(params[:garage_id])
+    @reservations = @garage.reservations
   end
 
   def new
-    # add it to routes!
     @reservation = Reservation.new
   end
 
   def create
     @reservation = Reservation.create(reservation_params)
+    @reservation.garage = @garage
     if @reservation.save
       redirect_to reservation_path(@reservation)
     else
@@ -28,6 +30,10 @@ class ReservationsController < ApplicationController
   end
 
   private
+
+  def set_garage
+    @garage = Garage.find(params[:garage_id])
+  end
 
   def set_reservation
     @reservation = Reservation.find(params[:id])
