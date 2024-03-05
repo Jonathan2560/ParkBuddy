2729fe8d0c0d48a22f5872dcc680ef1eb1458cb2
Pauline van Rijckevorsel <vr.pauline@gmail.com>
2024-03-05 15:56:13 +0100

    adding the devises

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,5 @@
 class ReservationsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show]
   before_action :set_reservation, only: [:show, :destroy]
 
   def show
