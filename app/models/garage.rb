a52d9cbf981ab89d5621bb0e2017b652c733d95a
Jonathan Elst <jonathan.elst@outlook.com>
2024-03-06 12:26:29 +0100

    changed validation for garage from price_per_hour to price_per_minute

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -5,5 +5,5 @@ class Garage < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-  validates :address, :width, :length, :price_per_hour, :name, presence: true
+  validates :address, :width, :length, :price_per_minute, :name, presence: true
 end
