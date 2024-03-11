d63eb214ec1b6cb4af7677bc8eb3d740519fe392
Jonathan Elst <jonathan.elst@outlook.com>
2024-03-11 10:47:09 +0100

    added ngrok to view site locally on phone

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Adding phone local server using ngrok
+  config.hosts << /.*\.ngrok\.io$/
 end
