18fea0e0b836c8fc92268121e70def78dd97aa22
Jonathan Elst <jonathan.elst@outlook.com>
2024-03-13 13:13:07 +0100

    stripe implemented

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,18 +4,22 @@ class OrdersController < ApplicationController
   end
 
   def create
-    reservation = Reservation.find(params[:reservation_id]) # Is this the correct id, are we passing it in params?
+    reservation = Reservation.find(params[:reservation_id])
     order = Order.create!(reservation: reservation, amount: reservation.price, state: 'pending', user: current_user)
-
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [{
-        name: "Parking @#{reservation.garage.address}",
-        # images: [Cloudinary::Utils.cloudinary_url(reservation.garage.photo.key, :width => 100, :height => 150, :crop => :fill)],
-        amount: reservation.price_cents,
-        currency: 'eur',
+        price_data: {
+          currency: 'eur',
+          product_data: {
+            name: "Parking @#{reservation.garage.address}",
+            description: "From #{reservation.from}, until #{reservation.until}."
+          },
+          unit_amount: reservation.price_cents
+        },
         quantity: 1
       }],
+      mode: 'payment',
       success_url: order_url(order),
       cancel_url: order_url(order)
     )
